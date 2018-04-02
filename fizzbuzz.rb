4a8eb95df870018b8cfa5b00f1e6eee02b7805dd
Ji Yim <gsooyim@gmail.com>
2018-04-02 15:59:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
+  if int % 15 == 0
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 15 == 0
-    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
   end 
 end
